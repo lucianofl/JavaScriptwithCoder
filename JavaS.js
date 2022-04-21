133adc241d501cd8d5cd60bda988945fe8545c20
luchofl <lucianofleitas6@hotmail.com>
2022-04-21 01:09:54 -0300

    Ciclos desafio coder House

---
+++ JavaS.js
@@ -0,0 +1,12 @@
+let numero = parseInt(prompt("Ingrese un numero del 1 al 50 para saber cual es par"));
+
+while (numero <= 50){
+    if (numero % 2 == 0){
+        console.log(`${numero} - Este numero es par`);
+        numero++;
+        continue;
+    }
+    if(numero == 50) break;
+    console.log(numero);
+    numero++;
+}alert("Ingresa un numero entre 0 y 50")
